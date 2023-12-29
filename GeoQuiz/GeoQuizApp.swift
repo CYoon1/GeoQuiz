398a7ad7240ed897748bcb9e586be54227db9d53
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-12-29 11:01:09 -0800

    Initial Commit

---
+++ GeoQuiz/GeoQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoQuizApp.swift
+//  GeoQuiz
+//
+//  Created by Christopher Yoon on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct GeoQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
