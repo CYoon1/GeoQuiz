65568601a8dade00539e7e2c6fe251c7e12166da
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2024-01-02 13:40:22 -0500

    Created ListOfLists

---
+++ GeoQuiz/QuestionListOfLists.swift
@@ -0,0 +1,25 @@
+//
+//  QuestionListOfLists.swift
+//  GeoQuiz
+//
+//  Created by Christopher Yoon on 1/2/24.
+//
+
+import SwiftUI
+import Observation
+
+struct QuestionListOfLists: View {
+    var body: some View {
+        NavigationStack {
+            Text("Placeholder")
+                .navigationTitle("List of Lists")
+        }
+    }
+}
+
+// Need Add New List (Requires Name)
+// Display List of Lists
+
+#Preview {
+    QuestionListOfLists()
+}
